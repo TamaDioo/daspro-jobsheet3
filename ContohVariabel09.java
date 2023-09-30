f912cb21d2cd4bb8e1b23a9f40ba29fa199403eb
Tama Dioo <tamadio91@gmail.com>
2023-09-30 18:01:07 +0700

    Commit dan push hasil praktikum jobsheet 3

---
+++ ContohVariabel09.java
@@ -0,0 +1,15 @@
+public class ContohVariabel09  {
+
+   public static void main(String[] args) {
+   String salahSatuHobySayaAdalah = "Bermain game online";
+   boolean isPandai = true;
+   char jenisKelamin = 'L';
+   byte _umurSayaSekarang = 19;
+   double $ipk = 3.55, tinggi = 1.65;
+   System.out.println(salahSatuHobySayaAdalah);
+   System.out.println("Apakah pandai? " + isPandai);
+   System.out.println("Jenis kelamin: " + jenisKelamin);
+   System.out.println("Umurku saat ini " + _umurSayaSekarang);
+   System.out.println(String.format("Saya beripk %e, dengan tinggi badan %e", $ipk, tinggi));
+   }
+}
